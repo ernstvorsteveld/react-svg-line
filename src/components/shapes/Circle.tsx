6720097ce5ba067b969b693bec20a8b3b71cec5e
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2023-10-09 21:02:21 +0200

    move is enabled

--- src/components/shapes/Circle.tsx
+++ src/components/shapes/Circle.tsx
@@ -1,4 +1,4 @@
-import React, {forwardRef} from 'react';
+import React, {forwardRef, MouseEventHandler, useRef, useState} from 'react';
 
 export type CircleProps = {
     cx: number
@@ -11,13 +11,58 @@ export type CircleProps = {
 
 export const Circle: React.ForwardRefExoticComponent<CircleProps & React.RefAttributes<SVGCircleElement>> =
     forwardRef((props: CircleProps, circleRef: React.ForwardedRef<SVGCircleElement>) => {
-    return <circle className={props.className}
-        cx={props.cx}
-        cy={props.cy}
-        r={props.r}
-        stroke={props.stroke}
-        fill={props.fillColor}
-        ref={circleRef}
-    >
-    </circle>
-});
+
+        const [position, setPosition] =
+            useState({x: props.cx, y: props.cy, coords: {x: props.cx, y: props.cy}})
+
+        const handleMouseMove: React.MutableRefObject<(e: MouseEvent) => void> = useRef(e => {
+            console.log("handleMouseMove")
+
+            setPosition(position => {
+                const xDiff = position.coords.x - e.pageX
+                const yDiff = position.coords.y - e.pageY
+                return {
+                    x: position.x - xDiff,
+                    y: position.y - yDiff,
+                    coords: {
+                        x: e.pageX,
+                        y: e.pageY,
+                    },
+                }
+            });
+            document.addEventListener('mouseup', handleMouseUp);
+        });
+
+        const handleMouseDown: MouseEventHandler<SVGCircleElement> = e => {
+            console.log("handleMouseDown")
+            const pageX = e.pageX
+            const pageY = e.pageY
+            setPosition(position =>
+                Object.assign({}, position, {
+                    coords: {
+                        x: pageX,
+                        y: pageY,
+                    },
+                }),
+            )
+            document.addEventListener('mousemove', handleMouseMove.current);
+        }
+
+        const handleMouseUp = () => {
+            console.log("handleMouseUp")
+
+            document.removeEventListener('mousemove', handleMouseMove.current)
+            setPosition(position => Object.assign({}, position, {coords: {}}))
+        }
+
+        return <circle className={props.className}
+                       cx={position.x}
+                       cy={position.y}
+                       r={props.r}
+                       stroke={props.stroke}
+                       fill={props.fillColor}
+                       ref={circleRef}
+                       onMouseDown={handleMouseDown}
+        >
+        </circle>
+    });
